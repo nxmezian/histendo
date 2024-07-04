331dd435c218edbea6c693c60da2328e6fc384ae
Naoufal <naoufal.mezian@gmail.com>
2024-07-04 21:30:56 +0200

    04-07-2024 build

---
+++ src/UnderConstruction.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+import MainNav from './MainNav';
+
+const UnderConstruction = () => {
+    return (
+      <div className="under-construction"> 
+      <MainNav />
+      </div>
+      
+    );
+  };
+  
+  export default UnderConstruction;
