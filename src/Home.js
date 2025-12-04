d71204cc9247c82dfae620592fa06977a77d76b2
Naoufal <naoufal.mezian@gmail.com>
2025-12-04 19:06:40 +0100

    add CSS for smoother scrolling

--- src/Home.js
+++ src/Home.js
@@ -5,21 +5,21 @@ import HistendoLogo from './assets/images/histendo.png';
 export default function Home() {
   return (
     <>
-      <div className="fullscreen-section home-1">
+      <div className="fullscreen-section home-1 screen">
         <MainNav />
         <img src={HistendoLogo} id="histendo-logo" alt="Histendo Logo" loading="lazy"/>
-        <h1>We'll take you back in time and show you unforettable games from past</h1>
+        <h1>We'll take you back in time and show you unforgettable games from past</h1>
       </div>
 
-      <div className="fullscreen-section home-2">
+      <div className="fullscreen-section home-2 screen">
       <h1>through timelines</h1>
       </div>
 
-      <div className="fullscreen-section home-3">
+      <div className="fullscreen-section home-3 screen">
       <h1>amazing artwork</h1>
       </div>
 
-            <div className="fullscreen-section home-4">
+            <div className="fullscreen-section home-4 screen">
       <h1>and classic start screens!</h1>
       </div>
     </>
